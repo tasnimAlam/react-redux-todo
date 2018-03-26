d7825b25013cbc7b11f6c59492bd7b3b96154ad8
=tasnim alam <tasnimalamcse@gmail.com>
2018-03-26 23:18:29 +0600

    enable CORS in server, test data fetching

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => <div>Hello world</div>;
+class App extends Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    const url = "http://localhost:4000/all";
+    fetch(url)
+      .then(res => res.json())
+      .then(data => this.setState({ data }));
+  }
+
+  getTasks = () => {
+    const { data } = this.state;
+    return data.map(item => <li key={item.id}>{item.task}</li>);
+  };
+  
+  render() {
+    return (
+      <div>
+        <h3>Tasks </h3>
+        <ul>{this.getTasks()}</ul>
+      </div>
+    );
+  }
+}
 
 export default App;
--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const all = require("./routes/all");
 const completed = require("./routes/completed");
 const todos = require("./routes/todos");
 
+// CORS 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/all", all);
 app.use("/completed", completed);
 app.use("/todos", todos);
